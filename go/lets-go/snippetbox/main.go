7b61e503842c0ea21cb5cf3d841c090fb76c9b27
VergilX <abhinand.dmanoj@gmail.com>
2024-12-13 01:39:09 +0530

    go: snippetbox: http header-and-body

--- go/lets-go/snippetbox/main.go
+++ go/lets-go/snippetbox/main.go
@@ -1,28 +1,58 @@
 package main
 
 import (
+    "fmt"
     "log"
     "net/http"
+    "strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+    w.Header().Add("Server", "Go")
+
     w.Write([]byte("Hello from SnippetBox"))
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Display a specific snippet..."))
+    // Retrieve wildcard values r.PathValue()
+    // and error checking strconv
+    id, err := strconv.Atoi(r.PathValue("id"))  // voids non integers (Atoi)
+    if err != nil || id < 1 {
+        fmt.Println(err)
+        http.NotFound(w, r)
+        return
+    }
+
+    /*
+    // Sprintf allows interpolation with message (old way)
+    msg := fmt.Sprintf("Display a specific snippet with ID %d...", id)
+    w.Write([]byte(msg))
+    */
+
+    // use a function instead of primitive Write method
+    // uses interface io.Writer as argument
+    // so can use standard library functions like fmt
+    fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
     w.Write([]byte("Display a form for creating a new snippet..."))
 }
 
-func main() {
+func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+    // Write http header
+    w.WriteHeader(http.StatusCreated)
+
+    w.Write([]byte("Save a new snippet"))
+}
+
+func main() 
     mux := http.NewServeMux()  // one to many (multiplexer by regex)
 
-    mux.HandleFunc("/{$}", home)  // Adds handle "/"-> func home()
-    mux.HandleFunc("/snippet/view", snippetView)  
-    mux.HandleFunc("/snippet/create", snippetCreate)  
+    mux.HandleFunc("GET /{$}", home)  // Adds handle "/"-> func home()
+    mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+    mux.HandleFunc("GET /snippet/create", snippetCreate)
+    mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
     log.Print("starting server on :4000")
 
