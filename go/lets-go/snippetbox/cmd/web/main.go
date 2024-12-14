1f7ecf6168b2da2da2abeadcebe431ad81f96787
VergilX <abhinand.dmanoj@gmail.com>
2024-12-14 17:10:21 +0530

    go: snippetbox: static fileserver

--- go/lets-go/snippetbox/cmd/web/main.go
+++ go/lets-go/snippetbox/cmd/web/main.go
@@ -8,6 +8,14 @@ import (
 func main() {
     mux := http.NewServeMux()  // one to many (multiplexer by regex)
 
+    // Create a fileserver for static files
+    fileserver := http.FileServer(http.Dir("./ui/static/"))
+
+    // Handler for fileserver
+    // all URLs that start with /static/
+    // Arg taken is stripped of /static using http.StripPrefix
+    mux.Handle("GET /static/", http.StripPrefix("/static", fileserver))
+
     mux.HandleFunc("GET /{$}", home)  // Adds handle "/"-> func home()
     mux.HandleFunc("GET /snippet/view/{id}", snippetView)
     mux.HandleFunc("GET /snippet/create", snippetCreate)
