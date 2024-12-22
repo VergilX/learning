0e622d9f1999ff2ebece9c8090a3c4a0ed07e6e4
VergilX <abhinand.dmanoj@gmail.com>
2024-12-22 20:20:23 +0530

    go: snippetbox: db injection
    - main.go:      added func to open db connection pool
    - snippets.go:  db model structs
    - main.go:      db dependency injection using SnippetModel (from
      snippets.go)

--- go/lets-go/snippetbox/cmd/web/main.go
+++ go/lets-go/snippetbox/cmd/web/main.go
@@ -1,32 +1,50 @@
 package main
 
 import (
+    "database/sql"
     "flag"
     "log/slog"
     "net/http"
     "os"
+
+    "github.com/VergilX/learning/go/lets-go/snippetbox/internal/models"
+
+    _"github.com/go-sql-driver/mysql"
 )
 
-// all dependencies of app
+// all dependencies of app (dependency injection)
 type application struct {
     logger *slog.Logger
+    snippets *models.SnippetModel
 }
 
 func main() {
     // command line flags
     // flag function Args: 1-flagname; 2-defaultval; 3-desc
     addr := flag.String("addr", ":4000", "HTTP port address")
+    dsn := flag.String("dsn", "web:@/snippetbox?parseTime=true", "MySQL data source name")  // cmd for DB DSN String (Data Source Name)
 
-    // Right now addr has default value
+    // Right now cmd has default values
     // Get the actual value, if present
     flag.Parse()
     
     // Structured logger
     logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
+    // Connection pool to database
+    db, err := openDB(*dsn)
+    if err != nil {
+        logger.Error(err.Error())
+        os.Exit(1)
+    }
+
+    // defer close (delay until main is done)
+    defer db.Close()
+
     // application 'app' used for dependency injection
     app := &application {
-        logger: logger,
+        logger:     logger,
+        snippets:   &models.SnippetModel{db: DB},
     }
 
     // retrieve routes from routes.go into servemux
@@ -35,9 +53,25 @@ func main() {
 
     logger.Info("starting server", slog.String("addr", *addr))
 
-    err := http.ListenAndServe(*addr, mux)  // listens at port ("host:port")
+    err = http.ListenAndServe(*addr, mux)  // listens at port ("host:port")
     // if host not mentioned, goes an all network interfaces avaiable
 
     logger.Error(err.Error())
     os.Exit(1)
+}
+
+
+func openDB(dsn string) (*sql.DB, error) {
+    db, err := sql.Open("mysql", dsn)
+    if err != nil {
+        return nil, err
+    }
+
+    err = db.Ping()
+    if err != nil {
+        db.Close()
+        return nil, err
+    }
+
+    return db, nil
 }
