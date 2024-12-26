ba96e37d220bc562317abdd9108b7a8b87564daa
VergilX <abhinand.dmanoj@gmail.com>
2024-12-26 20:39:01 +0530

    go: snippetbox: snippet Get() function
    Single-record SQL queries

--- go/lets-go/snippetbox/cmd/web/handlers.go
+++ go/lets-go/snippetbox/cmd/web/handlers.go
@@ -1,78 +1,84 @@
 package main
 
 import (
-    "fmt"
-    "html/template"
-    "net/http"
-    "strconv"
+	"errors"
+	"fmt"
+	"html/template"
+	"net/http"
+	"strconv"
+
+	"github.com/VergilX/learning/go/lets-go/snippetbox/internal/models"
 )
 
-
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-    w.Header().Add("Server", "Go")
+	w.Header().Add("Server", "Go")
 
-    // files slice (base template should be *first*)
-    files := []string{
-        "./ui/html/base.tmpl",
-        "./ui/html/pages/home.tmpl",
-        "./ui/html/partials/nav.tmpl",
-    }
+	// files slice (base template should be *first*)
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/pages/home.tmpl",
+		"./ui/html/partials/nav.tmpl",
+	}
 
-    // ts: Template set
-    // takes files slice
-    // ... is used to pass slice elements as args
-    ts, err := template.ParseFiles(files...)
-    if err != nil {
-        app.serverError(w, r, err)  // helpers.go
-        return
-    }
+	// ts: Template set
+	// takes files slice
+	// ... is used to pass slice elements as args
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, r, err) // helpers.go
+		return
+	}
 
-    // Add template to http body
-    // second arg: name of template ( {{define "base"}} )
-    // last arg: dynamic data (nil for now)
-    err = ts.ExecuteTemplate(w, "base", nil)
-    if err != nil {
-        app.serverError(w, r, err)  // helpers.go
-    }
+	// Add template to http body
+	// second arg: name of template ( {{define "base"}} )
+	// last arg: dynamic data (nil for now)
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		app.serverError(w, r, err) // helpers.go
+	}
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-    // Retrieve wildcard values r.PathValue()
-    // and error checking strconv
-    id, err := strconv.Atoi(r.PathValue("id"))  // voids non integers (Atoi)
-    if err != nil || id < 1 {
-        fmt.Println(err)
-        http.NotFound(w, r)
-        return
-    }
+	// Retrieve wildcard values r.PathValue()
+	// and error checking strconv
+	id, err := strconv.Atoi(r.PathValue("id")) // voids non integers (Atoi)
+	if err != nil || id < 1 {
+		fmt.Println(err)
+		http.NotFound(w, r)
+		return
+	}
 
-    /*
-    // Sprintf allows interpolation with message (old way)
-    msg := fmt.Sprintf("Display a specific snippet with ID %d...", id)
-    w.Write([]byte(msg))
-    */
+	// Retrieve snippet from db using model
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			http.NotFound(w, r)
+		} else {
+			app.serverError(w, r, err)
+		}
 
-    // use a function instead of primitive Write method
-    // uses interface io.Writer as argument
-    // so can use standard library functions like fmt
-    fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+		return
+	}
+
+	fmt.Fprintf(w, "%+v", snippet)
+
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Display a form for creating a new snippet..."))
+	w.Write([]byte("Display a form for creating a new snippet..."))
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-    // Write http header
-    title := "0 snail"
-    content := "0 snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n- Kobayashi Issa"
-    expires := 7
+	// Write http header
+	title := "0 snail"
+	content := "0 snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n- Kobayashi Issa"
+	expires := 7
 
-    id, err := app.snippets.Insert(title, content, expires)
-    if err != nil {
-        app.serverError(w, r, err)
-        return
-    }
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
 
-        http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
--- go/lets-go/snippetbox/internal/models/snippets.go
+++ go/lets-go/snippetbox/internal/models/snippets.go
@@ -2,6 +2,7 @@ package models
 
 import (
     "database/sql"
+    "errors"
     "time"
 )
 
@@ -36,7 +37,24 @@ func (m *SnippetModel) Insert(title string, content string, expires int) (int, e
 }
 
 func (m *SnippetModel) Get(id int) (Snippet, error) {
-    return Snippet{}, nil
+    stmt := `SELECT id, title, content, created, expires FROM snippets
+    WHERE expires > UTC_TIMESTAMP() AND id = ?`
+
+    row := m.DB.QueryRow(stmt, id)
+
+    var s Snippet
+
+    err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+    if err != nil {
+        if errors.Is(err, sql.ErrNoRows) {
+            return Snippet{}, ErrNoRecord
+        } else {
+            return Snippet{}, err
+        }
+    }
+
+    // If everything went ok
+    return s, nil
 }
 
 func (m *SnippetModel) Latest() ([]Snippet, error) {
