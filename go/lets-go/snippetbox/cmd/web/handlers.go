5353d918ab1ffb245205d3150c0761a9c70b91ec
VergilX <abhinand.dmanoj@gmail.com>
2024-12-31 13:54:48 +0530

    go: snippetbox: snippetView UI
    - need to pass single snippet struct to frontend template
    - passed snippet wrapped in templateData struct (only single object can
      be passed)
    - rendered object in snippetview

--- go/lets-go/snippetbox/cmd/web/handlers.go
+++ go/lets-go/snippetbox/cmd/web/handlers.go
@@ -3,7 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
-	// "html/template"
+	"html/template"
 	"net/http"
 	"strconv"
 
@@ -70,8 +70,30 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "%+v", snippet)
+    files := []string{
+        "./ui/html/base.tmpl",
+        "./ui/html/partials/nav.tmpl",
+        "./ui/html/pages/view.tmpl",
+    }
 
+
+    ts, err := template.ParseFiles(files...)
+    if err != nil {
+        app.serverError(w, r, err)
+        return
+    }
+
+    // wrap data in a single struct. Defined in templates.go
+    data := templateData{
+        Snippet: snippet,
+    }
+
+    // pass snippet data during execution of template (models.Snippet)
+    // wrapped by templateData
+    err = ts.ExecuteTemplate(w, "base", data)
+    if err != nil {
+        app.serverError(w, r, err)
+    }
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
---
+++ go/lets-go/snippetbox/cmd/web/templates.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/VergilX/learning/go/lets-go/snippetbox/internal/models"
+
+type templateData struct {
+    Snippet models.Snippet
+}
