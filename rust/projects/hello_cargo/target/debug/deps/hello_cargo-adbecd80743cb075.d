ab2ed7ca09efb93ec2cff8658b1b5d20852d078b
abhinand <abhinand.dmanoj@gmail.com>
2025-10-01 23:44:58 +0530

    rust: hello_world

---
+++ rust/projects/hello_cargo/target/debug/deps/hello_cargo-adbecd80743cb075.d
@@ -0,0 +1,5 @@
+/home/abhinand/Abhinand/code/learn/rust/projects/hello_cargo/target/debug/deps/hello_cargo-adbecd80743cb075: src/main.rs
+
+/home/abhinand/Abhinand/code/learn/rust/projects/hello_cargo/target/debug/deps/hello_cargo-adbecd80743cb075.d: src/main.rs
+
+src/main.rs:
---
+++ rust/projects/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/abhinand/Abhinand/code/learn/rust/projects/hello_cargo/target/debug/hello_cargo: /home/abhinand/Abhinand/code/learn/rust/projects/hello_cargo/src/main.rs
